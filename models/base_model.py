6b23b6bbe714b3db191966b88cd192de047917ea
Tariq Yunusa <tariikhyunusa@gmail.com>
2023-05-10 19:36:22 +0000

    updated base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -6,11 +6,27 @@ from datetime import datetime
 
 class BaseModel:
     """This class defines a base model"""
-    def __init__(self):
-        """Initialization method"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        """Initialization method
+	 Args:
+	    - *args: list of arguments
+	    - **kwargs: dict of key-values arguments
+	"""
+
+	if kwargs is not None and kwargs != {}:
+	    for key in kwargs:
+	        if key == "created_at":
+		    self.__dict__["created_at"] = datetime.strptime(
+			kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+		elif key == "updated_at":
+		    self.__dict__["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+		else:
+		    self.__dict__[key] = kwargs[key]
+	    else:
+       		 self.id = str(uuid.uuid4())
+       		 self.created_at = datetime.now()
+       		 self.updated_at = datetime.now()
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
