defe9e77dae4abdec86ae63b64d30f46d1574ecb
segunlawal <lawalsegun72@gmail.com>
2023-05-10 17:52:39 -0700

    update BaseModel class

--- models/base_model.py
+++ models/base_model.py
@@ -8,25 +8,22 @@ class BaseModel:
     """This class defines a base model"""
     def __init__(self, *args, **kwargs):
         """Initialization method
-	 Args:
-	    - *args: list of arguments
-	    - **kwargs: dict of key-values arguments
-	"""
+        Args:
+            *args: list of arguments
+            **kwargs: dict of key-values arguments
+        """
 
-	if kwargs is not None and kwargs != {}:
-	    for key in kwargs:
-	        if key == "created_at":
-		    self.__dict__["created_at"] = datetime.strptime(
-			kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
-		elif key == "updated_at":
-		    self.__dict__["updated_at"] = datetime.strptime(
-                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
-		else:
-		    self.__dict__[key] = kwargs[key]
-	    else:
-       		 self.id = str(uuid.uuid4())
-       		 self.created_at = datetime.now()
-       		 self.updated_at = datetime.now()
+        if kwargs is not None and kwargs != {}:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key in ('created_at', 'updated_at'):
+                        setattr(self, key, datetime.fromisoformat(value))
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
