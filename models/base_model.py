6b12058a142fbaa241ab43183d1593918130aec0
segunlawal <lawalsegun72@gmail.com>
2023-05-12 10:55:54 -0700

    add missing obj

--- models/base_model.py
+++ models/base_model.py
@@ -25,7 +25,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new()
+            storage.new(self)
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
